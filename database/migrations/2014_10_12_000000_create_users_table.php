4113118a8a09faf222a91600f4f8c85bba03e047
Singgih <gokonggokong71@gmail.com>
2020-08-29 08:26:26 +0700

    setup model and migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string("username")->unique();
+            $table->string("roles");
+            $table->text("address");
+            $table->string("phone");
+            $table->enum("status", ["ACTIVE", "INACTIVE"]);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
