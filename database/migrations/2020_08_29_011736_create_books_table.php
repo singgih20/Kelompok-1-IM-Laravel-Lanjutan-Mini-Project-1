d9cc832a2434ac2eafe220b91cfdc9cee71c069d
Singgih <gokonggokong71@gmail.com>
2020-08-29 08:34:24 +0700

    redesign book table

--- database/migrations/2020_08_29_011736_create_books_table.php
+++ database/migrations/2020_08_29_011736_create_books_table.php
@@ -21,12 +21,9 @@ class CreateBooksTable extends Migration
             $table->string('author');
             $table->string('publisher');
             $table->float('price');
-            $table->integer('views')->default(0)->unsigned();
             $table->integer('stock')->default(0)->unsigned();
             $table->enum('status', ['PUBLISH', 'DRAFT']);
             $table->integer('created_by');
-            $table->integer('updated_by')->nullable();
-            $table->integer('deleted_by')->nullable();
 
             $table->timestamps();
         });
